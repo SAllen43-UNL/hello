2209e0f65fd277e8a2eb8332e5084f8f25d8f805
SAllen43-UNL <sallen43@huskers.unl.edu>
2025-08-28 12:42:25 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+ * Author:  Stanley Allen
+ *          sallen43@huskers.unl.edu
+ * Date: 2025/08/28
+ *
+ * A simple C program that prints my name
+ */
+
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ #define NAME "Stanley Allen"
+
+ int main(int argc, char**argv) {
+
+    printf("%s\n", NAME);
+
+    return 0;
+ }
